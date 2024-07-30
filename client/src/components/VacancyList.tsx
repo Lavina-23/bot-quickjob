0bfc96bea52efab161caa31ed8c9bee0fdea8789
Lavina-23 <lavinakohai3227@gmail.com>
2024-07-30 09:26:57 +0700

    feat: display data to website

--- client/src/components/VacancyList.tsx
+++ client/src/components/VacancyList.tsx
@@ -2,10 +2,10 @@ import { useEffect, useState } from "react";
 import { fetchVacancy } from "../services/api";
 
 interface Vacancy {
-  id: number,
-  name: string,
-  job_title: string,
-  sallary: number,
+  _id: string;
+  name: string;
+  job_title: string;
+  sallary: number;
 }
 
 const VacancyList = () => {
@@ -19,18 +19,22 @@ const VacancyList = () => {
     getVacancies();
   }, []);
 
-  return <div>
-    <h2>Lowongan Tersedia</h2>
-    <ul>
-      {vacancies && vacancies.length ? vacancies.map((vacancy) => (
-        <div>
-          <h3>${vacancy.job_title}</h3>
-          <h3>${vacancy.sallary}</h3>
-          <p>Dibuat oleh: ${ vacancy.name }</p>
-        </div>
-      )) : null}
-    </ul>
-  </div>;
-}
+  return (
+    <div>
+      <h2>Lowongan yang Tersedia</h2>
+      <ul>
+        {vacancies && vacancies.length
+          ? vacancies.map((vacancy) => (
+              <div key={vacancy._id}>
+                <h3>Pekerjaan: {vacancy.job_title}</h3>
+                <h3>Sallary: {vacancy.sallary}</h3>
+                <p>Dibuat oleh: {vacancy.name}</p>
+              </div>
+            ))
+          : null}
+      </ul>
+    </div>
+  );
+};
 
 export default VacancyList;
--- client/src/services/api.ts
+++ client/src/services/api.ts
@@ -4,7 +4,8 @@ const API_URL = import.meta.env.VITE_API_URL;
 
 export const fetchVacancy = async () => {
   try {
-    const response = await axios.post(`${API_URL}/post`);
+    const response = await axios.get(`${API_URL}/getVacancy`);
+    console.log(response.data);
     return response.data;
   } catch (error) {
     console.error("Error fetching vacancies:", error);
